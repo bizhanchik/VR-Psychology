ec82e46cc4370bdcd7005a093a9befc8f80db940
GODtouch <79089500+GODtouch@users.noreply.github.com>
2025-05-14 15:13:33 +0500

    Bug fixes
    Some bugs with assets are fixed

--- Assets/Scenes/KidneyScript.cs
+++ Assets/Scenes/KidneyScript.cs
@@ -1,15 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class KidneyScript : MonoBehaviour
 {
     public Transform kidneyObject;
+    private Animator kidneyAnimator;
+
+    private void Start()
+    {
+        if (kidneyObject != null)
+        {
+            kidneyAnimator = kidneyObject.GetComponent<Animator>();
+        }
+    }
 
     public void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
+            if (kidneyAnimator != null)
+            {
+                kidneyAnimator.SetBool("isNeedMove", true);
+            }
+
             kidneyObject.gameObject.SetActive(false);
         }
     }
